a8d32c8b95972ff3b932f56bca3aabafe46bbd00
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-17 22:54:04 -0500

    Create variables and roll method

--- src/main/kotlin/Die.kt
+++ src/main/kotlin/Die.kt
@@ -1,3 +1,11 @@
-class Die {
+import kotlin.random.Random
 
+class Die {
+    var name : String = ""
+    var noOfSides : Int = 0
+    var randomSideUp : Int = 0
+
+    private fun roll(){
+        randomSideUp = Random.nextInt(1, noOfSides+1)
+    }
 }
